fe182b8e7215d6d737aba829722ad47e4d6b0da8
Ben <ben.shamas@live.com>
2022-03-25 22:05:52 -0400

    Finished most of main page

---
+++ dingers-restaurant-prototype/js/app.js
@@ -0,0 +1,37 @@
+//Changes the styling of the Navbar after you scroll down
+$(function () {
+    $(document).on('scroll', () => {
+        var $top_bar = $(".top-bar");
+        var $nav = $(".navbar-custom");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $top_bar.height());
+    });
+});
+//Adds the return to top button after scrolling
+$(function () {
+    $(document).on('scroll', () => {
+        var $home = $("#home");
+        var $returnTopButton = $(".back-to-top");
+        if ($(this).scrollTop() > $home.height() / 3) {
+            $returnTopButton.css("display", "initial")
+        }
+        else {
+            $returnTopButton.css("display", "none")
+        }
+
+    });
+});
+// Adds or removes the active link class for the Navbar
+$(function () {
+    $(".nav-link").click(function () {
+        console.log("hello world")
+        // remove classes from all
+        $(".nav-link").removeClass("active");
+        // add class to the one we clicked
+        $(this).addClass("active");
+    });
+});
+// $("#contactbtn").click(function () {
+//     $('html,body').animate({
+//         scrollTop: $("#footer").offset().top
+//     }, 5000);
+// });
