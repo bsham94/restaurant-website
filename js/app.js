d29f71ea862eb48fa2d4a6aa44f18ff100561f73
Ben <ben.shamas@live.com>
2022-04-14 17:04:34 -0400

    fixed navbar

--- js/app.js
+++ js/app.js
@@ -1,70 +1,27 @@
-//Changes the styling of the Navbar after you scroll down
-// $(function () {
-//     changeNavColour();
-// });
-// $(function () {
-//     $(document).on('scroll', () => {
-//         changeNavColour();
+// Adds or removes the active link class for the Navbar
+// $(() => {
+//     $(".nav-link").on('click', (e) => {
+//         // remove classes from all
+//         $(".nav-link").removeClass("active");
+//         // add class to the one we clicked
+//         $(e.target).addClass("active");
 //     });
 // });
-
-// Adds or removes the active link class for the Navbar
-$(() => {
-    $(".nav-link").on('click', (e) => {
-        // remove classes from all
-        $(".nav-link").removeClass("active");
-        // add class to the one we clicked
-        $(e.target).addClass("active");
-    });
-});
 //Smooth Scroll
 $(() => {
     changeNavColour();
-    showBackToTop()
+    showBackToTop();
     $(document).on("scroll", onScroll);
-    $('.navbar-custom a[href^="#"]').on('click', function (e) {
-        e.preventDefault();
-        $(document).off("scroll");
-
-        $('.nav-link').each(function () {
-            $(this).removeClass('active');
-        })
-        $(this).addClass('active');
-        //Gets href part of a tag
-        var target = this.hash;
-        $target = $(target);
-        $('html, body').stop().animate(
-            {
-                'scrollTop': $target.offset().top + 5
-            },
-            {
-                duration: 700,
-                easing: 'swing',
-                progress: (a, p, mR) => {
-                    if (mR <= 550) {
-                        if ($(".navbar-custom scrolled")) {
-                            console.log("hello world");
-                            $(".navbar-custom").addClass("scrolled");
-                        }
-                    }
-                },
-                complete: () => {
-                    window.location.hash = target;
-                    $(document).on("scroll", onScroll);
-                }
-            });
-
-    });
 });
 
 onScroll = () => {
     changeNavColour();
     showBackToTop();
-    var scrollPos = $(document).scrollTop();
+    var scrollPos = $(window).scrollTop();
     $('.navbar-custom a').each(function () {
         var currLink = $(this);
         var refElement = $(currLink.attr("href"));
-        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() >= scrollPos) {
+        if (refElement.position().top - 100 <= scrollPos && refElement.position().top + refElement.height() >= scrollPos) {
             $('.navbar-custom ul li a').removeClass("active");
             currLink.addClass("active");
         }
@@ -73,6 +30,7 @@ onScroll = () => {
         }
     });
 }
+
 //Change Navbar Colour
 changeNavColour = () => {
     $(".navbar-custom").toggleClass('scrolled', $(this).scrollTop() > $(".top-bar").height());
@@ -136,3 +94,44 @@ $(() => {
 });
 
 
+//Smooth Scroll
+// $(() => {
+//     changeNavColour();
+//     showBackToTop()
+//     $(document).on("scroll", onScroll);
+//     $('.navbar-custom a[href^="#"]').on('click', function (e) {
+//         e.preventDefault();
+//         $(document).off("scroll");
+
+//         $('.nav-link').each(function () {
+//             $(this).removeClass('active');
+//         })
+//         $(this).addClass('active');
+//         //Gets href part of a tag
+//         var target = this.hash;
+//         $target = $(target);
+//         $('html, body').stop().animate(
+//             {
+//                 'scrollTop': $target.offset().top + 5
+//             },
+//             {
+//                 duration: 700,
+//                 easing: 'swing',
+//                 step: (a, b) => {
+//                     changeNavColour();
+//                 },
+//                 progress: (a, p, mR) => {
+//                     if (mR <= 550) {
+//                         if ($(".navbar-custom scrolled")) {
+//                             $(".navbar-custom").addClass("scrolled");
+//                         }
+//                     }
+//                 },
+//                 complete: () => {
+//                     window.location.hash = target;
+//                     $(document).on("scroll", onScroll);
+//                 }
+//             });
+
+//     });
+// });
